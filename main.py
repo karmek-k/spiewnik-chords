80ae172e3453dfd1f2f9d8c5cfefe57dd2047c24
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-11-20 19:15:02 +0100

    Create soup objects and scrape chords

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import bs4
 import requests
 
 
@@ -9,3 +10,13 @@ status = res.status_code
 if status >= 400:
     print(f'Bad status code: {status}')
     exit(1)
+
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+chords = []
+for codeTag in soup.find_all('code'):
+    chord = f'{codeTag["data-chord"]}{codeTag["data-suffix"]}'
+    if codeTag['data-chord'] and chord not in chords:
+        chords.append(chord)
+
+print(chords)
