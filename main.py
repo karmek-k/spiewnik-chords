b498c5db846e1ee256d76fd6a2e49f9691024aff
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-11-21 12:32:12 +0100

    Set the artist to something less problematic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from scraper.artist import ArtistPage
 from scraper.song import Song
 
-page = ArtistPage('https://spiewnik.wywrota.pl/szanty')
+page = ArtistPage('https://spiewnik.wywrota.pl/kombii')
 for song in page.scrape():
     print(song)
